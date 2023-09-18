74f14dc6f0fc1f8174399816ded702ccf6cdfbea
Kunji <kunji.m@myparkplus.com>
2023-09-18 12:17:53 +0530

    native plus flutter

--- Netflix Clone/Controllers/Core/HomeViewController.swift
+++ Netflix Clone/Controllers/Core/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Flutter
 
 enum Sections: Int {
     case TrendingMovies = 0
@@ -41,6 +42,20 @@ class HomeViewController: UIViewController {
         homeFeedTable.tableHeaderView = headerView
         
         configureHeroHeaderView()
+        
+        let button = UIButton(type:UIButton.ButtonType.custom)
+            button.addTarget(self, action: #selector(showFlutter), for: .touchUpInside)
+            button.setTitle("Show Flutter!", for: UIControl.State.normal)
+            button.frame = CGRect(x: 80.0, y: 210.0, width: 160.0, height: 40.0)
+            button.backgroundColor = UIColor.blue
+            self.view.addSubview(button)
+    }
+    
+    @objc func showFlutter() {
+      let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
+      let flutterViewController =
+          FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+      present(flutterViewController, animated: true, completion: nil)
     }
     
     private func configureHeroHeaderView(){
